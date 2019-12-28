a971db7b60dd8d9f1de004833eb4ed2c87832953
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-28 17:34:26 +0900

    Two

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,10 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    'django_seed',
+    'rest_framework',
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
